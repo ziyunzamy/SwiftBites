a4a23ffc6267fe73f476de2ccd9339680bfd4071
Ziyun Zheng <ziyunz@andrew.cmu.edu>
2016-11-07 13:18:11 -0500

    Initial Commit

---
+++ SwiftBitesTests/SwiftBitesTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftBitesTests.swift
+//  SwiftBitesTests
+//
+//  Created by Ziyun Zheng on 11/7/16.
+//  Copyright © 2016 Ziyun Zheng. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftBites
+
+class SwiftBitesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
